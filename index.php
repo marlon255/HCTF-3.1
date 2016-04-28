07ab87fcace62e312b7a070630d3ff8d66064fed
marlon255 <marlon.0255@gmail.com>
2016-04-28 17:21:08 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>HCTF 3.1</title>
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div id="topo">
+		<div id="logo">
+		</div>
+		<h2>Heaven Capture The Flag</h2>
+	</div>
+	<div id="login">
+		<form method="post">
+			<fieldset>
+				<legend>Login</legend>
+				<input type="text" name="" id="" placeholder="Login" class="text" >
+				<input type="password" name="" id="" placeholder="Password" class="text" >
+				<input type="submit" name="" id="" value="Logar" class="submit" >
+			</fieldset>
+		</form>
+	</div>
+	<div id="cadastro" onClick="location.href='cadastro.php';">Cadastrar</div>
+</body>
+</html>
